2400eff1a48ee5e721cdb3638ec2d85cf52f1363
yuz <re.yuz77777@gmail.com>
2020-02-19 15:30:24 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/yuzuy/pkgo
+
+go 1.13
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.0
+	github.com/pkg/browser v0.0.0-20180916011732-0a3d74bf9ce4
+	github.com/urfave/cli/v2 v2.1.1
+)
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/pkg/browser"
+	"github.com/urfave/cli/v2"
+)
+
+const pkgGoDevUrl = "https://pkg.go.dev/"
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "pkgo"
+	app.Usage = "Find package and open the document"
+
+	app.Action = func(c *cli.Context) error {
+		if 0 < c.NArg() {
+			pkg := c.Args().Get(0)
+			exist, err := isExistPage(pkgGoDevUrl + pkg)
+			if err != nil {
+				return err
+			}
+			if exist {
+				if err := browser.OpenURL(pkgGoDevUrl + pkg); err != nil {
+					return err
+				}
+				return nil
+			}
+			exist, err = isExistPage(pkgGoDevUrl + "golang.org/x/" + pkg)
+			if err != nil {
+				return err
+			}
+			if exist {
+				if err := browser.OpenURL(pkgGoDevUrl + "golang.org/x/" + pkg); err != nil {
+					return err
+				}
+				return nil
+			}
+			exist, err = isExistPage(pkgGoDevUrl + "github.com/" + pkg)
+			if err != nil {
+				return err
+			}
+			if exist {
+				if err := browser.OpenURL(pkgGoDevUrl + "github.com/" + pkg); err != nil {
+					return err
+				}
+				return nil
+			}
+			fmt.Println("Cannot find this package.")
+			return nil
+		}
+		if err := browser.OpenURL(pkgGoDevUrl); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func isExistPage(url string) (bool, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return false, err
+	}
+	if res.StatusCode == 404 {
+		return false, nil
+	}
+	return true, nil
+}
